34a35ecf4270407772dbfb5e3cbb6c3c25f938f5
Rosy Tucker <rosy533@gmail.com>
2014-10-02 17:08:41 +0100

    Fix Tests

--- test/example/test/routes.clj
+++ test/example/test/routes.clj
@@ -1,14 +1,14 @@
-(ns ask.test.routes
+(ns example.test.routes
   (:require [clojure.test :refer :all]
-            [ask.routes :refer :all]
+            [example.routes :refer :all]
             [ring.mock.request :as mock]
-            [ask.templates :as templates]))
+            [example.templates :as templates]))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) (templates/render-template "home" {:name "Rosy"})))))
+      (is (= (:body response) (templates/render-template "home" {:message "Hello World"})))))
   
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
